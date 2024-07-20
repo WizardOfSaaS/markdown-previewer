e8b06046b3692587b497b5389868a973e2fdd81a
WizardOfSaaS <WizardOfSaaS@proton.me>
2024-07-20 16:50:11 +0530

    Add HTML sanitation

--- app/page.js
+++ app/page.js
@@ -4,17 +4,19 @@ import Editor from "./components/Editor"
 import Previewer from "./components/Previewer"
 import { marked } from "marked"
 import ViewBar from "./components/ViewBar"
+import DOMPurify from "dompurify"
 
 export default function Home() {
   const [input, setInput] = useState('')
   const [layout, setLayout] = useState('BOTH')
   const convertedHtml = marked(input)
+  const cleanedHtml = DOMPurify.sanitize(convertedHtml, { USE_PROFILES: { html: true } })
   return (
     <main className="flex">
       <ViewBar layout={layout} setLayout={setLayout} />
       <div className="editor-previewer">
         {layout !== 'PREVIEW' && <Editor input={input} setInput={setInput} />}
-        {layout !== 'EDITOR' && <Previewer html={convertedHtml} />}
+        {layout !== 'EDITOR' && <Previewer html={cleanedHtml} />}
       </div>
     </main>
   )
