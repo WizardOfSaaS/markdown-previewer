214db5b9850f0703d320446496b48cc10ffac3d1
WizardOfSaaS <WizardOfSaaS@proton.me>
2024-07-20 14:45:56 +0530

    Add Viewbar component

---
+++ app/components/ViewBar.jsx
@@ -0,0 +1,12 @@
+"use client";
+function ViewBar() {
+  return (
+    <div className="viewbar">
+      <button>EDITOR</button>
+      <button>BOTH</button>
+      <button>PREVIEW</button>
+    </div>
+  );
+}
+
+export default ViewBar;
--- app/page.js
+++ app/page.js
@@ -3,14 +3,18 @@ import { useState } from "react"
 import Editor from "./components/Editor"
 import Previewer from "./components/Previewer"
 import { marked } from "marked"
+import ViewBar from "./components/ViewBar"
 
 export default function Home() {
   const [input, setInput] = useState('')
   const convertedHtml = marked(input)
   return (
     <main className="flex">
-      <Editor input={input} setInput={setInput} />
-      <Previewer html={convertedHtml} />
+      <ViewBar />
+      <div className="editor-previewer">
+        <Editor input={input} setInput={setInput} />
+        <Previewer html={convertedHtml} />
+      </div>
     </main>
   )
 }
