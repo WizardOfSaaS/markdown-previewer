f8c5c693bbb45ef20051fdc991698ad335858d35
WizardOfSaaS <WizardOfSaaS@proton.me>
2024-07-20 16:33:48 +0530

    Add footer

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <div id="footer"><a target="_blank" href="https://github.com/WizardOfSaaS/markdown-previewer">Source Code</a> by <a target="_blank" href="https://x.com/WizardOfSaaS">WizardOfSaaS</a></div>
+      </body>
     </html>
   );
 }
